5bfe6d7424bad940ef213b7b1009bc78be250f16
dev-t2 <oo_ri@naver.com>
2022-09-22 23:24:29 +0900

    React Navigation and Emotion Theme Type Update

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,19 @@
 import { DefaultTheme, DarkTheme, Theme as NavigationTheme } from '@react-navigation/native';
 
-type Theme = {};
+const theme = {
+  colors: {},
+};
 
-export type CustomTheme = NavigationTheme & Theme;
+export type CustomTheme = NavigationTheme & typeof theme;
 
 export const lightTheme: CustomTheme = {
   ...DefaultTheme,
+  ...theme,
+  colors: { ...DefaultTheme.colors, ...theme.colors },
 };
 
 export const darkTheme: CustomTheme = {
   ...DarkTheme,
+  ...theme,
+  colors: { ...DarkTheme.colors, ...theme.colors },
 };
