210c73e362a7f219a23e07184307f3a510370d97
dev-t2 <oo_ri@naver.com>
2022-10-23 11:45:49 +0900

    Movie App Repository Reset

--- src/emotion.d.ts
+++
@@ -1,7 +0,0 @@
-import '@emotion/react';
-
-import { CustomTheme } from './theme';
-
-declare module '@emotion/react' {
-  export interface Theme extends CustomTheme {}
-}
--- src/screens/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Movie } from './Movie';
-export { default as Search } from './Search';
-export { default as Tv } from './Tv';
