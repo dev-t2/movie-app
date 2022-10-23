d034cef14045e4c7d5f6cb78138169d19f7405c6
dev-t2 <oo_ri@naver.com>
2022-10-23 13:34:43 +0900

    Splash Screen Install and Setup

--- App.tsx
+++ App.tsx
@@ -1,15 +1,38 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 
+preventAutoHideAsync();
+
 const App = () => {
-  return (
-    <View>
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await new Promise((resolve: (value?: any) => void) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (isReady) {
+      await hideAsync();
+    }
+  }, [isReady]);
+
+  return isReady ? (
+    <View onLayout={onLayout}>
       <StatusBar style="auto" />
 
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  ) : null;
 };
 
 export default memo(App);
