76c5b8f3c42297e623d6298240bebb22e0813083
dev-t2 <oo_ri@naver.com>
2022-10-23 16:28:06 +0900

    Emotion Install and Theme Setup

--- App.tsx
+++ App.tsx
@@ -1,24 +1,31 @@
 import React, { memo, useMemo } from 'react';
+import { useColorScheme } from 'react-native';
 import { useAssets } from 'expo-asset';
 import { useFonts } from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
+import { ThemeProvider } from '@emotion/react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import BottomTab from './src/navigators/BottomTab';
+import { darkTheme, lightTheme } from './src/theme';
 import { Splash } from './src/screens';
+import BottomTab from './src/navigators/BottomTab';
 
 const App = () => {
+  const colorScheme = useColorScheme();
   const [isAssets] = useAssets([]);
   const [isFonts] = useFonts({ ...Ionicons.font });
 
   const isReady = useMemo(() => isAssets && isFonts, [isAssets, isFonts]);
+  const theme = useMemo(() => (colorScheme === 'light' ? lightTheme : darkTheme), [colorScheme]);
 
-  return isReady ? (
-    <NavigationContainer>
-      <BottomTab />
-    </NavigationContainer>
-  ) : (
+  return !isReady ? (
     <Splash />
+  ) : (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <BottomTab />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
